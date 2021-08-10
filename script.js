63fc41b194c42e4004cde4d0a7274673aef4f321
NoobieUs37 <NoobieUs37@protonmail.com>
2021-08-10 09:07:08 +0530

    Make mobile perfect responsive

--- script.js
+++ script.js
@@ -5,5 +5,6 @@ const wordCount = document.getElementById('word-count');
 inputTextArea.addEventListener('input', () => {
     charCount.textContent = inputTextArea.value.length;
     let txt = inputTextArea.value.trim();
+    console.log(txt);
     wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
 });
